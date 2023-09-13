d295f3fa396924e8476112a2639c1142df4cda24
niscitacan12 <citacandrarini@gmail.com>
2023-09-13 15:55:16 +0700

    membuat home page dan login page di codeigniter

---
+++ application/helpers/my_helper.php
@@ -0,0 +1,13 @@
+<?php
+// untuk menampilkan tingkat kelas dan jurusan kelas by id
+function tampilan_full_kelas_byid($id)
+{
+    $ci =& get_instance();
+    $ci->load->database();
+    $result = $ci->db->where('id', $id)->get('kelas');
+    foreach ($result->result() as $c) {
+        $stmt = $c->tingkat_kelas . ' ' . $c->jurusan_kelas; // Perbaiki bagian ini
+        return $stmt;
+    }
+}
+?>
---
+++ application/views/home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Home</title>
+    <!-- Menggunakan Bootstrap CSS dari CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css">
+     <style>
+            body {
+                background-image : url('https://i.pinimg.com/736x/82/cd/60/82cd60c48f91022bfcdf7affc1ce53a8.jpg');
+                background-size : cover;
+                background-repeat : no-repeat;
+                background-attachment : fixed;
+                
+            }
+        </style>
+</head>
+<body>
+    <header class="bg-primary text-white text-center py-4">
+    <div class="text-center">
+         <img src="https://binusasmg.sch.id/ppdb/logobinusa.png" alt="image" height="200px" width="250px" >
+        </div>
+        <h1>SMK Bina Nusantara</h1>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    </nav>
+    <main class="container mt-4">
+        <section class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">Identitas Sekolah</h3>
+                        <p class="card-text">Nama sekolah : SMK Bina Nusantara</p>
+                        <p class="card-text">Sekolah Nasional Jenjang : SMK</p>
+                        <p class="card-text">Pendidikan Status Sekolah : Swasta</p>
+                        <p class="card-text">Alamat Sekolah : JL.Mondosari No.5 MRANGGEN DEMAK</p>
+                        <p class="card-text">Kecamatan : Kec.Mranggen</p>
+                        <p class="card-text">Kabupaten : Kab.Demak</p>
+                        <p class="card-text">Provinsi : Prov.Jawa Tengah</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">Informasi Sekolah</h3>
+                        <p class="card-text">Akreditasi : C</p>
+                        <p class="card-text">Kurikulum : 2013</p>
+                        <p class="card-text">Kepala Sekolah : Rif'atul Khoer</p>
+                        <p class="card-text">Operator Data : Sugiyarto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">Sarana Prasarana</h3>
+                        <p class="card-text">1.Ruang kelas</p>
+                        <p class="card-text">2.Ruang guru</p>
+                        <p class="card-text">3.Ruang ibadah</p>
+                        <p class="card-text">4.Ruang toilet</p>
+                        <p class="card-text">5.Tempat bermain olahraga</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mt-4">
+            <h2>SMK Bina Nusantara Mranggen</h2>
+            <ul>
+                <li>jurusan SMK cocok untuk siswa perempuan</li>
+                <li>mengenal jurusan teknik komputer dan jaringan</li>
+                <li>jurusan SMK sesuai era revolusi industri</li>
+            </ul>
+        </section>
+        <div class= "jumbotron text-center">
+        <a class="btn btn-primary" href="./auth" role="button">Login</a>
+        </div>
+    </main>
+    <footer class="bg-dark text-white text-center py-3 mt-4">
+        &copy; 2023 Your Company Name
+    </footer>
+
+    <!-- Menggunakan Bootstrap JS dari CDN (opsional, hanya jika Anda memerlukan komponen Bootstrap yang interaktif) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
