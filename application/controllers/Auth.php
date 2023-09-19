b1f212dc67bb798c51c2c8a5768f891c995b906a
niscitacan12 <citacandrarini@gmail.com>
2023-09-19 16:25:57 +0700

    menambahkan CRUD siswa dan guru

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,37 +15,6 @@ class Auth extends CI_Controller {
 		$this->load->view('auth/login');
 	}
 
-	public function aksi_login()
-	{
-		$email = $this->input->post('email', true);
-		$password = $this->input->post('password', true);
-		$data = [ 'email' => $email, ];
-		$query = $this->m_model->getwhere('admin', $data);
-		$result = $query->row_array();
-
-		if (!empty($result) && md5($password) === $result['password']) {
-			$data = [
-				'logged_in' => TRUE,
-				'email' => $result['email'],
-				'username' => $result['username'],
-				'role' => $result['role'],
-				'id' => $result['id'],
-			];
-			$this->session->set_userdata($data);
-			if ($this->session->userdata('role') == 'admin') {
-				redirect(base_url()."admin");
-			} else {
-				redirect(base_url()."auth");
-			}
-		} else {
-			redirect(base_url()."auth");
-		}
-	}
-
-	function logout() {
-		$this->session->sess_destroy(); // Menggunakan sess_destroy() untuk mengakhiri sesi
-		redirect(base_url('auth'));
-	}
 	public function register()
     {
         $this->load->view('auth/register');
@@ -57,7 +26,6 @@ class Auth extends CI_Controller {
         $username = $this->input->post('username', true);
         $password = md5($this->input->post('password', true));
         $role = 'admin'; // Sesuaikan dengan role yang diinginkan
-
         $data = [
             'email' => $email,
             'username' => $username,
